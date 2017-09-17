c1171310aa77e9366d7220da6eab713aa5164839
Soravis Prakkamakul <asunnotthesun@gmail.com>
2017-09-17 00:51:45 -0700

    Added contact list

---
+++ LolMate-iOS/LolMate/ChatViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChatViewController.swift
+//  LolMate
+//
+//  Created by Sun on 9/17/2560 BE.
+//  Copyright © 2560 TCDisrupt. All rights reserved.
+//
+
+import Foundation
--- LolMate-iOS/LolMate/MatchedUserViewController.swift
+++ LolMate-iOS/LolMate/MatchedUserViewController.swift
@@ -9,22 +9,68 @@
 import UIKit
 import Hyphenate
 
-class MatchedUserViewController: UIViewController {
+let REUSE_IDENTIFIER = "MatchedUserCell"
+
+class MatchedUserViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var contacts:[Any]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         EMClient.shared().contactManager.getContactsFromServer { (contacts, error) in
-            print("got contacts")
+            print("-- got contacts")
             print("\(contacts!)")
+            self.contacts = contacts
+            self.tableView.reloadData()
         }
+        
+        tableView.dataSource = self
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.none
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(self.contacts != nil) {
+            return contacts!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: REUSE_IDENTIFIER)
+        let user = contacts?[indexPath.row]
+        print("\(user!)")
+        if(cell != nil) {
+            if let img = cell?.viewWithTag(1) as? UIImageView {
+                img.layer.cornerRadius = 25.0
+                
+            }
+            
+            if let lbl = cell?.viewWithTag(2) as? UILabel {
+                lbl.text = user! as! String
+                
+            }
+            
+        }
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //TODO: OpenChat
     }
 
 }
